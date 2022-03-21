ebbe39d6fe120f5e7d567c9098b103b9955086ca
Mateusz Szefler <szefler.mateusz91@gmail.com>
2022-03-21 13:33:15 +0100

    little update

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data;
+public class UserData
+{
+    private readonly ISqlDataAccess _db;
+
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<UserModel>> GetUsers() => _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+    
+    public async Task<UserModel?> GetUser(int id)
+    {
+        var results = await _db.LoadData<UserModel, dynamic>(
+            "dbo.spUser_get",
+            new { Id = id });
+        return results.FirstOrDefault();
+    }
+}
